efd8fce1ffefc3880889f658d65bdb19a2256ca1
Cleancookie <pokemon999101@hotmail.co.uk>
2022-01-22 16:27:19 +0000

    ✨ init Laravel & Inertia

---
+++ resources/js/Pages/MyPage.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+    <h1>{{ yerd }}</h1>
+</div>
+</template>
+
+<script>
+export default {
+    name: "my-page",
+    props: {
+        yerd: {
+            type: String
+        }
+    }
+};
+</script>
